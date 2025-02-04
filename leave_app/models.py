9abeff9da30f3dd82776f3ae6f51a5e864696d3c
Mihajaz <mihajarazafimahazoson@gmail.com>
2025-02-04 09:51:45 +0300

    models fonctionnel

--- leave_app/models.py
+++ leave_app/models.py
@@ -3,23 +3,23 @@ from viewflow.models import Process
 # Create your models here.
 
 #modèle employé
-class Employee:
-    nom = models.CharField(_(""), max_length=50)
-    email = models.EmailField(_(""), max_length=254)
-    role = models.CharField(_(""), max_length=50)
+class Employee(models.Model):
+    nom = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254)
+    role = models.CharField(max_length=50)
 
 #
-class LeaveRequest:
-    employee = models.ForeignKey("GestionDemande.Employee", verbose_name=_("Employé"), on_delete=models.CASCADE)
-    start_date = models.DateField(_(""), auto_now=False, auto_now_add=False)
-    end_date = models.DateField(_(""), auto_now=False, auto_now_add=False)
-    reason = models.TextField(_(""))
-    statut = models.CharField(_(""), max_length=50)
+class LeaveRequest(models.Model):
+    employee = models.ForeignKey('Employee', verbose_name="Employé", on_delete=models.CASCADE)
+    start_date = models.DateField(auto_now=False, auto_now_add=False)
+    end_date = models.DateField(auto_now=False, auto_now_add=False)
+    reason = models.TextField()
+    statut = models.CharField(max_length=50)
     
-class Approval:
-    leave_request = models.ForeignKey("gestionConge.LeaveRequest", verbose_name=_(""), on_delete=models.CASCADE)
-    responsable = models.CharField(_(""), max_length=50)
-    direction = models.CharField(_(""), max_length=50)
+class Approval(models.Model):
+    leave_request = models.ForeignKey('LeaveRequest', verbose_name=("Demande de congé"), on_delete=models.CASCADE)
+    responsable = models.CharField(max_length=50)
+    direction = models.CharField(max_length=50)
     
 
 
