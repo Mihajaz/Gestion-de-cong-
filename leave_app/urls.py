3ff42aff43da16abd9c63934c8d56e06926aef00
Mihajaz <mihajarazafimahazoson@gmail.com>
2025-02-04 11:08:55 +0300

    interface employe et maj templates

--- leave_app/urls.py
+++ leave_app/urls.py
@@ -5,6 +5,11 @@ from . import views
 urlpatterns = [
     path('', views.home , name='home'),
     path('validation/', views.validation , name='validation'),
-    
-    
+    path('register/', views.register , name='register'),
+    path('employe/', views.employe, name = 'employe'),
 ]  
+
+
+
+
+
--- leave_app/views.py
+++ leave_app/views.py
@@ -4,10 +4,19 @@ from django.shortcuts import render
 
 
 # Create your views here.
+
+#interface d'accueil
 def home(request):
     return render(request, 'home.html')
 
+#interface du directeur et manageur
 def validation(request):
     return render(request,'validation.html')
 
+#interface de login
+def register(request):
+    return render(request,'register.html')
+
+def employe(request):
+    return render(request,'employe.html')
 
